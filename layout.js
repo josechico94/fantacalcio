ce95a0b37fac4e0d0ac93c7a153b81d23992839c
josechico94 <64092499+josechico94@users.noreply.github.com>
2025-10-31 16:06:16 +0100

    Delete layout.js

--- layout.js
+++
@@ -1,4 +0,0 @@
-export const metadata = { title: 'Fantacalcio Minimal', description: 'Serie A stats & projections (Next.js single deploy)' }
-export default function RootLayout({ children }) {
-  return (<html lang="es"><body style={{ fontFamily: 'system-ui, sans-serif', margin: 0, padding: 0 }}>{children}</body></html>)
-}
